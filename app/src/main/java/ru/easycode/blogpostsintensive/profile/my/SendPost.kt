c922812a245da90a0ed16122928f45fa1cef7c1b
killahar <127596327+killahar@users.noreply.github.com>
2025-03-10 21:29:40 +0300

    bp-04-SendPost-ui

--- app/src/main/java/ru/easycode/blogpostsintensive/profile/my/SendPost.kt
+++ app/src/main/java/ru/easycode/blogpostsintensive/profile/my/SendPost.kt
@@ -15,8 +15,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ru.easycode.blogpostsintensive.R
 
 @Composable
 fun SendPost(handlePost : (String) -> Unit) {
@@ -31,7 +33,7 @@ fun SendPost(handlePost : (String) -> Unit) {
             onValueChange = {text = it.trim()},
             shape = RoundedCornerShape(10.dp),
             maxLines = 5,
-            label = { Text("Enter your text") },
+            label = { Text(stringResource(R.string.post_hint)) },
             modifier = Modifier.weight(1f)
         )
         Spacer(modifier = Modifier.width(8.dp))
