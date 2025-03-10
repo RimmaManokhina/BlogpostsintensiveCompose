0c55edca9c7da67e0c4e2568e35fb8babc2fe7f0
killahar <127596327+killahar@users.noreply.github.com>
2025-03-10 21:25:32 +0300

    bp-04-SendPost-ui

--- app/src/main/java/ru/easycode/blogpostsintensive/profile/my/SendPost.kt
+++ app/src/main/java/ru/easycode/blogpostsintensive/profile/my/SendPost.kt
@@ -1,21 +1,19 @@
 package ru.easycode.blogpostsintensive.profile.my
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,12 +22,16 @@ import androidx.compose.ui.unit.dp
 fun SendPost(handlePost : (String) -> Unit) {
     var text by rememberSaveable { mutableStateOf("") }
 
-    Row(modifier = Modifier.padding(16.dp)) {
-        TextField(
+    Row(
+        modifier = Modifier.padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OutlinedTextField(
             value = text,
             onValueChange = {text = it.trim()},
-            supportingText = { Text("Enter your text") },
-
+            shape = RoundedCornerShape(10.dp),
+            maxLines = 5,
+            label = { Text("Enter your text") },
             modifier = Modifier.weight(1f)
         )
         Spacer(modifier = Modifier.width(8.dp))
